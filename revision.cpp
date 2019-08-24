11a7c7c393a2b309040c22d6f1244a607e70a487
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-08-23 22:28:24 -0400

    update

--- revision.cpp
+++ revision.cpp
@@ -12,146 +12,6 @@
 using namespace space::multiThread;
 
 double INT_ERROR = 1e-13;
-/*
-size_t resolve_spin_num(std::string const &file_name, size_t base = 22) {
-    std::fstream init(file_name, std::fstream::in);
-    std::string line;
-    std::getline(init, line);
-
-    bool in_space = true;
-    size_t token_num = 0;
-
-    for (auto c : line) {
-        if (std::isspace(c)) {
-            in_space = true;
-        } else if (in_space) {
-            token_num++;
-            in_space = false;
-        }
-    }
-
-    if (token_num == base) {
-        return 0;
-    } else if (token_num == base + 3) {
-        return 1;
-    } else if (token_num == base + 6) {
-        return 2;
-    } else if (token_num == base + 9) {
-        return 3;
-    } else {
-        return 999;
-    }
-}
-
-
-template<size_t spin_num>
-void
-single_thread_job(std::string work_dir, ConcurrentFile input, size_t start_id, size_t end_id, ConcurrentFile output,
-                  ConcurrentFile log) {
-    using Container = std::array<double, 12>;
-
-    using stepper_type = boost::numeric::odeint::runge_kutta_fehlberg78<Container>;
-    double ini_dt = 0.1 * secular::consts::year;
-    secular::Task<spin_num> task;
-    for (;;) {
-        if (input >> task) {
-            if (start_id <= task.ctrl.id && task.ctrl.id <= end_id) {
-                secular::deg_to_rad(task.obt_args);
-
-                std::array<double, 12> inits;
-
-                if (task.ctrl.DA == true) {
-                    initilize_DA(inits, task.obt_args);
-                } else {
-                    initilize_SA(inits, task.obt_args);
-                }
-
-                secular::SecularArg<decltype(task.ctrl), spin_num> args{task.ctrl, task.obt_args.m1, task.obt_args.m2, task.obt_args.m3};
-
-                std::fstream fout_{work_dir + "trajectory_" + std::to_string(task.ctrl.id) + ".txt", std::fstream::out};
-
-                fout_ << std::fixed << std::setprecision(14);
-
-                double out_time_ = 0;
-
-
-                auto observer = [&](auto &data, double t) {
-                    if (t >= out_time_) {
-                        (fout_) << t << ' ';
-                        for (auto const &d : data) {
-                            (fout_) << d << ' ';
-                        }
-                        (fout_) << "\r\n";
-                        out_time_ += task.ctrl.dt_out;
-                    }
-                };
-                //
-                auto func = secular::Dynamic_dispatch<decltype(task.ctrl), decltype(args), Container>(task.ctrl, args);
-
-                //auto func = secular::Static_dispatch<decltype(task.ctrl), decltype(args), Container>(task.ctrl, args);
-
-                try {
-                    boost::numeric::odeint::integrate_adaptive(
-                            boost::numeric::odeint::make_controlled(int_error, int_error, stepper_type()), func, inits,
-                            0.0, task.ctrl.end_time, ini_dt, observer);
-                } catch (secular::StopFlag flag) {
-                    if (flag == secular::StopFlag::shrink) {
-                        //
-                    }
-                }
-            }
-        } else {
-            break;
-        }
-    }
-}
-
-
-int main(int argc, char **argv) {
-    std::string input_file_name;
-    std::string work_dir;
-    size_t start_task_id, end_task_id;
-
-    space::tools::read_command_line(argc, argv, input_file_name, start_task_id, end_task_id, work_dir);
-
-    const int dir_err = system(("mkdir -p " + work_dir).c_str());
-    if (dir_err == -1) {
-        std::cout << "Error creating directory!\n";
-        exit(1);
-    }
-
-    work_dir += "/";
-
-    auto input_file = make_thread_safe_fstream(input_file_name, std::fstream::in);
-
-    auto output_file = make_thread_safe_fstream(work_dir + "statistics.txt", std::fstream::out);
-
-    auto log_file = make_thread_safe_fstream(work_dir + "log.txt", std::fstream::out);
-
-    size_t spin_num = resolve_spin_num(input_file_name, 24);
-
-    std::cout << std::setprecision(15);
-
-    space::tools::Timer timer;
-    timer.start();
-    if (spin_num == 0) {
-        space::multiThread::auto_multi_thread(single_thread_job<0>, work_dir, input_file, start_task_id, end_task_id,
-                                              output_file, log_file);
-    } else if (spin_num == 1) {
-        space::multiThread::auto_multi_thread(single_thread_job<1>, work_dir, input_file, start_task_id, end_task_id,
-                                              output_file, log_file);
-    } else if (spin_num == 2) {
-        space::multiThread::auto_multi_thread(single_thread_job<2>, work_dir, input_file, start_task_id, end_task_id,
-                                              output_file, log_file);
-    } else if (spin_num == 3) {
-        space::multiThread::auto_multi_thread(single_thread_job<3>, work_dir, input_file, start_task_id, end_task_id,
-                                              output_file, log_file);
-    } else {
-        std::cout << "wrong spin number!\n";
-    }
-    std::cout << "\r\n Time:" << timer.get_time() << '\n';
-    return 0;
-}*/
 
 void get_line(std::fstream& is, std::string& str) {
     std::getline(is, str);
@@ -187,7 +47,13 @@ auto resolve_sim_type(std::string const& line) {
     constexpr static bool single_average{false};
     constexpr static bool double_average{true};
 
-    size_t id = std::stoi(line);
+    size_t id = 0;
+
+    if(token_num>0) {
+        id = std::stoi(line);
+    } else{
+        return std::make_tuple(id, double_average, 0u);
+    }
 
     switch(token_num) {
         case 22 :
@@ -240,13 +106,12 @@ constexpr size_t DT_OFFSET = 4;
 constexpr size_t CTRL_OFFSET = 5;
 constexpr size_t ARGS_OFFSET = 10;
 
-
 struct Traj_args{
     Traj_args(std::string const& work_dir, size_t task_id, double _dt)
       : dt{_dt},
         t_output{0},
         file{work_dir +  "output_" + std::to_string(task_id) + ".txt", std::fstream::out} {
-            file << std::setprecision(6);
+            file << std::setprecision(15);
         }
 
     void move_to_next_output(){ t_output += dt;}
@@ -317,7 +182,6 @@ void single_thread_job(std::string work_dir, ConcurrentFile input, size_t start_
                     call_ode_int<3>(DA, ctrl, init_args, 0.0, t_end, observer);
                 }
             }
-
         } catch (secular::StopFlag flag) {
             if(flag == secular::StopFlag::eof)
                 break;
