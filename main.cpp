be09d37dcd05852d3c3edc62f0cb34a55f9f9e76
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-09-05 11:46:02 -0400

    fix

---
+++ main.cpp
@@ -0,0 +1,246 @@
+
+#include <iostream>
+#include "secular.h"
+#include "observer.h"
+#include "SpaceHub/src/multi-thread/multi-thread.hpp"
+#include "SpaceHub/src/tools/config-reader.hpp"
+#include "SpaceHub/src/tools/timer.hpp"
+#include "boost/numeric/odeint.hpp"
+#include <iomanip>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace space::multiThread;
+
+double INT_ERROR = 1e-13;
+
+bool get_line(std::fstream &is, std::string &str) {
+    std::getline(is, str);
+    if (!is)
+        return false;
+    else
+        return true;
+}
+
+enum class SimArgT {
+	SA, DA, empty, wrong
+};
+
+auto resolve_sim_type(std::string const &line) {
+    bool in_space = true;
+    size_t token_num = 0;
+    for (auto c : line) {
+        if (std::isspace(c)) {
+            in_space = true;
+        } else if (in_space) {
+            token_num++;
+            in_space = false;
+        }
+    }
+    size_t id = 0;
+
+    if (token_num > 0) {
+        id = std::stoi(line);
+    } else {
+        return std::make_tuple(id, SimArgT::empty, 0u);
+    }
+
+    switch (token_num) {
+        case 20 :
+            return std::make_tuple(id, SimArgT::DA, 0u);
+        case 21 :
+            return std::make_tuple(id, SimArgT::SA, 0u);
+        case 23 :
+            return std::make_tuple(id, SimArgT::DA, 1u);
+        case 24 :
+            return std::make_tuple(id, SimArgT::SA, 1u);
+        case 26 :
+            return std::make_tuple(id, SimArgT::DA, 2u);
+        case 27 :
+            return std::make_tuple(id, SimArgT::SA, 2u);
+        case 29 :
+            return std::make_tuple(id, SimArgT::DA, 3u);
+        case 30 :
+            return std::make_tuple(id, SimArgT::SA, 3u);
+        default :
+            return std::make_tuple(static_cast<size_t>(0), SimArgT::wrong, 0u);
+    }
+}
+
+
+constexpr size_t CTRL_OFFSET = 3;
+constexpr size_t ARGS_OFFSET = 8;
+
+enum class ReturnFlag {
+    input_err, max_iter, finish
+};
+
+auto args_analy(size_t& start_id, size_t& end_id, std::string const&f){
+    if(start_id <=0 || end_id <=0) {
+        std::cout << "task id cannot be smaller than 1!\r\n";
+        exit(0);
+    }
+
+    if(start_id > end_id)
+      std::swap(start_id, end_id);
+
+    size_t task_num = end_id - start_id + 1;
+
+    size_t thread_num = std::min(task_num, space::multiThread::auto_thread);
+
+    return std::make_tuple(task_num, thread_num);
+}
+
+template<size_t spin_num>
+auto call_ode_int(std::string work_dir, ConcurrentFile output, bool is_double_ave, secular::Controler const &ctrl, std::vector<double> const &init_args) {
+    using namespace boost::numeric::odeint;
+
+    using Container = secular::SecularArray<spin_num>;
+    //using stepper_type = boost::numeric::odeint::runge_kutta_fehlberg78<Container>;
+    using stepper_type = bulirsch_stoer<Container>;
+
+    auto  [task_id, t_end, out_dt] = secular::cast_unpack<decltype(init_args.begin()), size_t, double, double>(init_args.begin());
+
+    //space::display(std::cout, task_id, t_end, out_dt);
+
+    auto const [m1, m2, m3, a_in_init] = secular::unpack_args<4>(init_args.begin() + ARGS_OFFSET);
+
+    std::fstream f_out;
+
+    if (secular::is_on(out_dt)) {
+        f_out.open(work_dir + "output_" + std::to_string(task_id) + ".txt", std::fstream::out);
+        f_out << std::setprecision(12);
+    }
+
+    Container data;
+
+    initilize_orbit_args(is_double_ave, spin_num, data, init_args.begin() + ARGS_OFFSET);
+
+    secular::SecularConst<spin_num> const_parameters{m1, m2, m3};
+
+    double dt = 0.1 * secular::consts::year;
+
+    double time = 0;
+
+    stepper_type stepper{INT_ERROR, INT_ERROR};
+
+    secular::Stream_observer writer{f_out, out_dt};
+
+    secular::SMA_Determinator stop{const_parameters.a_in_coef(), ctrl.stop_a_in()};
+
+    auto func = secular::Dynamic_dispatch<Container>(ctrl, const_parameters);
+    writer(data, time);
+
+    //STATIC_DISPATH(ctrl, const_parameters,
+
+    for( ;time <= t_end && !stop(data, time); ) {
+        constexpr size_t max_attempts = 500;
+
+        controlled_step_result res = success;
+        size_t trials = 0;
+        do{
+            res = stepper.try_step(func, data, time, dt);
+            trials++;
+        } while((res == fail) && (trials < max_attempts));
+
+        if(trials == max_attempts){
+            return ReturnFlag::max_iter;
+        }
+        writer(data, time);
+    }
+    //)
+
+    output << PACK(task_id, ' ', time, ' ', data, "\r\n");
+    output.flush();
+
+    return ReturnFlag::finish;
+}
+
+void single_thread_job(std::string work_dir, ConcurrentFile input, size_t start_id, size_t end_id, ConcurrentFile output, ConcurrentFile log) {
+    std::string entry;
+    for (;input.execute(get_line, entry);) {
+            auto[task_id, AveType, spin_num] = resolve_sim_type(entry);
+
+			bool is_double_ave{ true };
+			if (AveType == SimArgT::DA) {
+				is_double_ave = true;
+			} else if (AveType == SimArgT::SA) {
+				is_double_ave = false;
+			} else if (AveType == SimArgT::empty) {
+				continue;
+			} else if (AveType == SimArgT::wrong) {
+				std::cout << "task" + std::to_string(task_id) + ":wrong input format!\r\n";
+				continue;
+			}
+
+            if (start_id <= task_id && task_id <= end_id) {
+                std::vector<double> v;
+
+                secular::unpack_args_from_str(entry, v, is_double_ave, spin_num);
+
+                secular::Controler ctrl{v.begin() + CTRL_OFFSET, is_double_ave};
+
+                log << secular::get_log_title(task_id, is_double_ave, ctrl, spin_num) + "\r\n";
+                log.flush();
+
+                ReturnFlag res;
+                if (spin_num == 0) {
+                    res = call_ode_int<0>(work_dir, output, is_double_ave, ctrl, v);
+                } else if (spin_num == 1) {
+                    res = call_ode_int<1>(work_dir, output, is_double_ave, ctrl, v);
+                } else if (spin_num == 2) {
+                    res = call_ode_int<2>(work_dir, output, is_double_ave, ctrl, v);
+                } else if (spin_num == 3) {
+                    res = call_ode_int<3>(work_dir, output, is_double_ave, ctrl, v);
+                }
+
+                if(res == ReturnFlag::max_iter)
+                    log << std::to_string(task_id) + ":Max iteration number reaches!\n";
+                    log.flush();
+            }
+    }
+}
+
+int main(int argc, char **argv) {
+    std::ios::sync_with_stdio(false);
+
+    std::string input_file_name;
+    std::string work_dir;
+    size_t start_task_id, end_task_id;
+    std::string user_specified_core_num;
+
+    space::tools::read_command_line(argc, argv, user_specified_core_num, input_file_name, start_task_id, end_task_id, work_dir);
+
+    auto [task_num, thread_num] = args_analy(start_task_id, end_task_id, input_file_name);
+
+    if(user_specified_core_num != "auto") {
+        if(secular::is_number(user_specified_core_num)){
+            thread_num = std::stoi(user_specified_core_num);
+        } else {
+            std::cout << "wrong format of the first argument(cpu core number)!\n";
+            exit(0);
+        }
+    }
+
+    const int dir_err = system(("mkdir -p " + work_dir).c_str());
+    if (dir_err == -1) {
+        std::cout << "Error creating directory!\n";
+        return 0;
+    }
+
+    work_dir += "/";
+
+    auto input_file = make_thread_safe_fstream(input_file_name, std::fstream::in);
+
+    auto output_file = make_thread_safe_fstream(work_dir + "last_state.txt", std::fstream::out);
+
+    auto log_file = make_thread_safe_fstream(work_dir + "log.txt", std::fstream::out);
+
+    std::cout << task_num << " tasks in total will be processed.    " << thread_num << " threads will be created for computing!" << std::endl;
+
+    space::tools::Timer timer;
+    timer.start();
+    space::multiThread::multi_thread_run(thread_num, single_thread_job, work_dir, input_file, start_task_id, end_task_id, output_file, log_file);
+    std::cout << "\r\n Time:" << timer.get_time() << " s\n";
+    return 0;
+}
---
+++ tools.h
@@ -0,0 +1,290 @@
+#ifndef SECULAR_TOOL_H
+#define SECULAR_TOOL_H
+
+#include <cmath>
+#include <tuple>
+#include <vector>
+#include <sstream>
+namespace secular {
+
+#define STD_ACCESSOR(TYPE, NAME, MEMBER)                                                                                 \
+  inline TYPE & NAME () {                                                                                                \
+    return MEMBER;                                                                                                       \
+  };                                                                                                                     \
+  inline TYPE const & NAME () const {                                                                                    \
+    return MEMBER;                                                                                                       \
+  };
+
+#define READ_GETTER(TYPE, NAME, MEMBER)                                                                                  \
+  inline TYPE const & NAME () const {                                                                                    \
+    return MEMBER;                                                                                                       \
+  };
+
+#define OPT_STD_GETTER(COND, TYPE, NAME, MEMBER)                                                                         \
+  inline TYPE & NAME () {                                                                                                \
+    static_assert(COND, "method is not defined!");                                                                       \
+    return MEMBER;                                                                                                       \
+  };                                                                                                                     \
+  inline TYPE const & NAME () const {                                                                                    \
+    static_assert(COND, "method is not defined!");                                                                       \
+    return MEMBER;                                                                                                       \
+  };
+
+#define OPT_READ_GETTER(COND, TYPE, NAME, MEMBER)                                                                        \
+  inline TYPE const & NAME () const {                                                                                    \
+    static_assert(COND, "method is not defined!");                                                                       \
+    return MEMBER;                                                                                                       \
+  };
+
+
+#define STD_3WAY_SETTER(NAME, X, Y, Z)                                                                                   \
+inline void set_##NAME(double x, double y, double z) {                                                                   \
+    X = x, Y = y, Z = z;                                                                                                 \
+}                                                                                                                        \
+inline void add_##NAME(double x, double y, double z) {                                                                   \
+    X += x, Y += y, Z += z;                                                                                              \
+}                                                                                                                        \
+inline void sub_##NAME(double x, double y, double z) {                                                                   \
+    X -= x, Y -= y, Z -= z;                                                                                              \
+}                                                                                                                        \
+inline void set_##NAME(std::tuple<double, double, double> const& t) {                                                    \
+    X = std::get<0>(t), Y = std::get<1>(t), Z = std::get<2>(t);                                                          \
+}                                                                                                                        \
+inline void add_##NAME(std::tuple<double, double, double> const& t) {                                                    \
+    X += std::get<0>(t), Y += std::get<1>(t), Z += std::get<2>(t);                                                       \
+}                                                                                                                        \
+inline void sub_##NAME(std::tuple<double, double, double> const& t) {                                                    \
+    X -= std::get<0>(t), Y -= std::get<1>(t), Z -= std::get<2>(t);                                                       \
+}
+
+#define OPT_3WAY_SETTER(COND, NAME, X, Y, Z)                                                                             \
+inline void set_##NAME(double x, double y, double z) {                                                                   \
+    static_assert(COND, "method is not defined!");                                                                       \
+    X = x, Y = y, Z = z;                                                                                                 \
+}                                                                                                                        \
+inline void add_##NAME(double x, double y, double z) {                                                                   \
+    static_assert(COND, "method is not defined!");                                                                       \
+    X += x, Y += y, Z += z;                                                                                              \
+}                                                                                                                        \
+inline void sub_##NAME(double x, double y, double z) {                                                                   \
+    static_assert(COND, "method is not defined!");                                                                       \
+    X -= x, Y -= y, Z -= z;                                                                                              \
+}                                                                                                                        \
+inline void set_##NAME(std::tuple<double, double, double> const& t) {                                                    \
+    static_assert(COND, "method is not defined!");                                                                       \
+    X = std::get<0>(t), Y = std::get<1>(t), Z = std::get<2>(t);                                                          \
+}                                                                                                                        \
+inline void add_##NAME(std::tuple<double, double, double> const& t) {                                                    \
+    static_assert(COND, "method is not defined!");                                                                       \
+    X += std::get<0>(t), Y += std::get<1>(t), Z += std::get<2>(t);                                                       \
+}                                                                                                                        \
+inline void sub_##NAME(std::tuple<double, double, double> const& t) {                                                    \
+    static_assert(COND, "method is not defined!");                                                                       \
+    X -= std::get<0>(t), Y -= std::get<1>(t), Z -= std::get<2>(t);                                                       \
+}
+
+template<typename Iter, size_t ...I>
+inline auto _unpack_array_(Iter iter, std::index_sequence<I...>) {
+    return std::make_tuple(*(iter+I)...);
+}
+
+template<size_t num, typename Iter>
+inline auto unpack_args(Iter iter) {
+    return _unpack_array_(iter, std::make_index_sequence<num>());
+}
+
+template<typename Iter, typename Tup, size_t ...I>
+inline auto _cast_unpack_(Iter iter, Tup const& t, std::index_sequence<I...>) {
+    return std::make_tuple(static_cast<decltype(std::get<I>(t))>( *(iter+I) )...);
+}
+
+template<typename Iter, typename ...Args>
+inline auto cast_unpack(Iter iter) {
+    return _cast_unpack_(iter, std::make_tuple(static_cast<Args>(0)...), std::make_index_sequence<sizeof...(Args)>());
+    //return std::make_tuple(static_cast<Args>(*iter++)...);
+}
+
+void unpack_args_from_str(std::string const &str, std::vector<double> &vec, bool DA, size_t spin_num) {
+    std::stringstream is{str};
+
+    size_t token_num = 20 + static_cast<size_t>(!DA) + spin_num * 3;
+
+    double tmp;
+
+    vec.reserve(token_num);
+    for (size_t i = 0; i < token_num; ++i) {
+        is >> tmp;
+        vec.emplace_back(tmp);
+    }
+}
+
+#define UNPACK3(x) std::get<0>(x), std::get<1>(x), std::get<2>(x)
+
+namespace consts {
+        constexpr double pi = 3.14159265358979323;
+        constexpr double G = 4 * pi * pi;
+        constexpr double C = 6.32397263e4;
+        constexpr double r_G_sqrt = 1.0 / (2 * pi);
+
+        constexpr double year = 1;
+}
+
+bool is_number(const std::string& s)
+{
+    return !s.empty() && std::find_if(s.begin(),
+        s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
+}
+
+bool is_on(double x) {
+    return x > 5e-15;
+}
+
+    template<typename Container>
+    struct spin_num {
+        static constexpr size_t size{Container::s_num};
+    };
+
+    using Tup3d = std::tuple<double, double, double>;
+
+    inline double norm2(double x, double y, double z) {
+        return x * x + y * y + z * z;
+    }
+
+    inline double norm2(Tup3d const &tup) {
+        return norm2(UNPACK3(tup));
+    }
+
+    inline double norm(double x, double y, double z) {
+        return sqrt(norm2(x, y, z));
+    }
+
+    inline double norm(Tup3d const &tup) {
+        return sqrt(norm2(tup));
+    }
+
+    inline double dot(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return x1 * x2 + y1 * y2 + z1 * z2;
+    }
+
+    inline double dot(Tup3d const &t1, Tup3d const &t2) {
+        return dot(UNPACK3(t1), UNPACK3(t2));
+    }
+
+    inline auto cross(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return std::make_tuple(y1 * z2 - y2 * z1, z1 * x2 - z2 * x1, x1 * y2 - x2 * y1);
+    }
+
+    inline auto cross(Tup3d const &t1, Tup3d const &t2) {
+        return cross(UNPACK3(t1), UNPACK3(t2));
+    }
+
+    inline auto cross_with_coef(double A, double x1, double y1, double z1, double x2, double y2, double z2) {
+        return std::make_tuple(A * (y1 * z2 - y2 * z1), A * (z1 * x2 - z2 * x1), A * (x1 * y2 - x2 * y1));
+    }
+
+    inline auto cross_with_coef(double A, Tup3d const &t1, Tup3d const &t2) {
+        return cross_with_coef(A, UNPACK3(t1), UNPACK3(t2));
+    }
+
+    double calc_angular_mom(double m_in, double m_out, double a) {
+        double mu = m_in * m_out / (m_in + m_out);
+        return mu * sqrt(consts::G * (m_in + m_out) * a);
+    }
+
+    auto unit_j(double i, double Omega) {
+        double sini = sin(i);
+        double cosi = cos(i);
+        double sinO = sin(Omega);
+        double cosO = cos(Omega);
+        return std::make_tuple(sini * sinO, -sini * cosO, cosi);
+    }
+
+    auto unit_e(double i, double omega, double Omega) {
+        double sini = sin(i);
+        double cosi = cos(i);
+        double sinO = sin(Omega);
+        double cosO = cos(Omega);
+        double sino = sin(omega);
+        double coso = cos(omega);
+
+        return std::make_tuple(coso * cosO - sino * cosi * sinO, coso * sinO + sino * cosi * cosO, sino * sini);
+    }
+
+    auto unit_peri_v(double i, double omega, double Omega) {
+        auto[ex, ey, ez] = unit_e(i, omega, Omega);
+        auto[jx, jy, jz] = unit_j(i, Omega);
+        return cross(jx, jy, jz, ex, ey, ez);
+    }
+
+
+    template<typename ...Args>
+    void deg_to_rad(Args &...args) {
+        constexpr double rad = consts::pi / 180.0;
+        ((args *= rad), ...);
+    }
+
+    inline auto calc_orbit_args(double Coef, double lx, double ly, double lz, double ex, double ey, double ez) {
+        double e_sqr = norm2(ex, ey, ez);
+
+        double j_sqr = fabs(1 - e_sqr);
+
+        double j = sqrt(j_sqr);
+
+        double L_norm = norm(lx, ly, lz);
+
+        double L = L_norm / j;
+
+        double a = Coef * L * L;
+
+        return std::make_tuple(e_sqr, j_sqr, j, L_norm, L, a);
+    }
+
+    inline auto calc_a_eff(double Coef, double lx, double ly, double lz, double ex, double ey, double ez) {
+        double e_sqr = norm2(ex, ey, ez);
+
+        double j_sqr = fabs(1 - e_sqr);
+
+        double j = sqrt(j_sqr);
+
+        double L_sqr = norm2(lx, ly, lz);
+
+        return Coef * L_sqr / j;
+    }
+
+    inline auto calc_a_j(double Coef, double lx, double ly, double lz, double ex, double ey, double ez) {
+        double e_sqr = norm2(ex, ey, ez);
+
+        double j_sqr = fabs(1 - e_sqr);
+
+        double j = sqrt(j_sqr);
+
+        double L_sqr = norm2(lx, ly, lz);
+
+        return std::make_tuple(Coef * L_sqr / j_sqr, j);
+    }
+
+    inline auto calc_a(double Coef, double lx, double ly, double lz, double ex, double ey, double ez) {
+        double e_sqr = norm2(ex, ey, ez);
+
+        double j_sqr = fabs(1 - e_sqr);
+
+        double L_sqr = norm2(lx, ly, lz);
+
+        return Coef * L_sqr / j_sqr;
+    }
+
+    const std::string str_ave[2] = {":SA", ":DA"};
+    const std::string str_pole[2] = {"|quad", "| oct"};
+    const std::string str_gr[2] = {"", "|GR"};
+    const std::string str_gw[2] = {"", "|GW"};
+    const std::string str_sl[2] = {"", "|S_{in}L_{out}"};
+    const std::string str_ll[2] = {"", "|LL"};
+    const std::string str_s[4] = {"", "|S_{1}L_{in}", "|S_{1}L_{in}|S_{2}L_{in}", "|S_{1}L_{in}|S_{2}L_{in}|S_{3}L_{out}"};
+
+    template<typename Controler>
+    std::string get_log_title(size_t task_id, bool DA, Controler const &ctrl, size_t spin_num) {
+        return std::to_string(task_id) + str_ave[DA] + str_pole[ctrl.Oct] + str_gr[ctrl.GR] + str_gw[ctrl.GW] + str_sl[ctrl.SL] + str_ll[ctrl.LL] +
+               str_s[spin_num];
+    }
+}
+#endif
